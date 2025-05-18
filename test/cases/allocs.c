e9794d6d830b75fe84fa460f81cfde6e4642bea5
Ultra-Code <mega.alpha100@gmail.com>
2025-05-18 17:52:46 +0000

    allocs.c: disable unsafe-buffer-handling fns warning
    add NOLINT_UNSAFE_BUFFER_HANDLING to disable warning that recommend
    annex k functions with *_s suffixes
    
    Warn about unused .ie remove -Wno-unused-macros
    
    fix all lint issues
    
    add include stdint.h for strict integer types
    
    Signed-off-by: Ultra-Code <mega.alpha100@gmail.com>

--- test/cases/allocs.c
+++ test/cases/allocs.c
@@ -1,27 +1,47 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#if defined(__clang__)
+#ifdef __clang_analyzer__
+/* Suppress security.insecureAPI.DeprecatedOrUnsafeBufferHandling warning
+ * https://clang.llvm.org/docs/analyzer/user-docs/FAQ.html#id10
+ */
+#define NOLINT_UNSAFE_BUFFER_HANDLING [[clang::suppress]]
+#else
+#define NOLINT_UNSAFE_BUFFER_HANDLING
+#endif
+#elif defined(__GNUC__)
+#define NOLINT_UNSAFE_BUFFER_HANDLING
+#else
+#define NOLINT_UNSAFE_BUFFER_HANDLING
+#endif
 
 extern void checkLeaks();
 
 int main() {
-  auto size = 100U;
-  int *arr = malloc(size * sizeof(int)); // Uses Zig allocator
+  auto const m_size = 100U * sizeof(uint32_t);
+  uint32_t *arr = malloc(m_size); // Uses Zig allocator
+
   if (!arr) {
-    if (fprintf(stderr, "Allocation failed!\n") < 0) {
+    NOLINT_UNSAFE_BUFFER_HANDLING
+    if (fprintf(stderr, "Allocation failed!\n") < 0) { // NOLINT
       perror("fprintf failed");
     }
     return 1;
   }
-  arr[0] = 42;
-  printf("Allocated: %d\n", arr[0]);
+  arr[0] = (uint32_t)m_size;
+  printf("Allocated: %ud\n", arr[0]);
 
-  int *new_arr = realloc(arr, 200 * sizeof(int)); // Resize safely
+  auto const new_size = 200U;
+  int *new_arr = realloc(arr, new_size * sizeof(int)); // Resize safely
   free(new_arr); // Correctly freed via Zig's allocator
 
-  void *ptr;
+  void *ptr = nullptr;
   auto const alignment = 64U;
+  auto const memalign_size = 1024U;
   // Allocate 64B-aligned memory for AVX-512
-  if (posix_memalign(&ptr, alignment, 1024) != 0) {
+  if (posix_memalign(&ptr, alignment, memalign_size) != 0) {
     perror("posix_memalign failed");
     return 1;
   }
